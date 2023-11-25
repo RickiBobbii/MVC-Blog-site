20de34d45e6ab44243ebc5ee98dc3cca2e6058ab
RickiBobbii <rickibobbiii@gmail.com>
2023-11-25 18:38:24 -0500

    got server running and added assets

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+//User for when User model is created
+const { User } = require('../../models');
+
+//Define api routes for User
+
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+// Import User model and withAuth 
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+//Add routes for User and other models
+
+
+module.exports = router;
