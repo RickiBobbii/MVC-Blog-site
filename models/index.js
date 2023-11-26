422a4b11952d13b15cc0a360402b98f5c6b610db
RickiBobbii <rickibobbiii@gmail.com>
2023-11-26 15:50:27 -0500

    updated seed and models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 //Grab User model to add relations
 const User = require('./User');
+const Blog = require('./Blog');
+
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+  
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 
-module.exports = { User };
+module.exports = { User, Blog };
