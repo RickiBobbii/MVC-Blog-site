4d44ef984f8b167fd5ae12e6e9f862d6e2aade92
RickiBobbii <rickibobbiii@gmail.com>
2023-11-26 18:03:17 -0500

    added api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const blogRoutes = require('./blogRoutes');
 
 //Add more routes
 router.use('/users', userRoutes);
+router.use('/blogs', blogRoutes);
 
 module.exports = router;
