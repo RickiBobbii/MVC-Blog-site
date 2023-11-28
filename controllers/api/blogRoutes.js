fd2b67633f33ea93a4a35dfc0a169351fff9aefb
RickiBobbii <rickibobbiii@gmail.com>
2023-11-28 17:25:16 -0500

    updated routes for crud

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Blog, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 //GET all blogs
@@ -15,6 +15,58 @@ router.get('/', async (req, res) => {
     }
   });
 
+//GET blog by id
+router.get('/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [{ model: User }],
+    });
+    if (!blogData) {
+      res.status(404).json({ message: 'No blog found with that id!' });
+      return;
+    }
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// UPDATE a blog by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!blogData[0]) {
+      res.status(404).json({ message: 'No blog with this id!'});
+      return;
+    }
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a blog
+router.delete('/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: 'No blog found with this id!' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});  
 router.post('/', withAuth, async (req, res) => {
   try {
     const newBlog = await Blog.create({
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 //User for when User model is created
-const { User } = require('../../models');
+const { User, Blog } = require('../../models');
 
 //Define api routes for User
 router.get('/', async (req, res) => {
@@ -15,6 +15,59 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+//GET user by id
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id, {
+      include: [{ model: Blog }],
+    });
+    if (!userData) {
+      res.status(404).json({ message: 'No User found with that id!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// UPDATE a user's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const userData = await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!userData[0]) {
+      res.status(404).json({ message: 'No user with this id!'});
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a user
+router.delete('/:id', async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post('/', async (req, res) => {
     try {
